a9de81a85701fb72de463c25cde00c07986bcda3
TaeYoonSS <lty0880@gmail.com>
2023-02-21 08:47:49 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
-<<<<<<< Updated upstream
+
 import sys
-<<<<<<< Updated upstream
 cnt = 0
 def mergeSort(arr) : 
     
     global cnt
-=======
-x666 666x x666 x666x 666xx x666x  
->>>>>>> Stashed changes
 
     if len(arr) < 2 :
         return arr
@@ -16,7 +12,6 @@ x666 666x x666 x666x 666xx x666x
     left_arr = mergeSort(arr[:mid])
     right_arr = mergeSort(arr[mid:])
 
-<<<<<<< Updated upstream
     l = r = 0
     tmp=[]
 
@@ -35,23 +30,11 @@ x666 666x x666 x666x 666xx x666x
         tmp += left_arr[l:]
     
     return tmp
-=======
-num = 666
-x = 0
-x += 1  
-num += x*1000
-if x = 9
-num *= 10
->>>>>>> Stashed changes
+
 
 input = sys.stdin.readline
 n, k = map(int,input().split())
 arr = list(map(int,input().split()))
 mergeSort(arr)
-=======
-n = int(sys.stdin.readline()) 
-arr = list(map(int,sys.stdin.readline().split()))
-result = sorted(list(set(arr)))
-for i in arr :
-    print(result.index(i),end=' ')
->>>>>>> Stashed changes
+
+
